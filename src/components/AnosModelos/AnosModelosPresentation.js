8edbabda444877b29953a048b6a09c1bd84b945f
rickboni80 <enriqueboni80@gmail.com>
2020-03-29 18:47:40 -0300

    Retirando spinner do AnosPresentations e CarroPresentation

--- src/components/AnosModelos/AnosModelosPresentation.js
+++ src/components/AnosModelos/AnosModelosPresentation.js
@@ -12,35 +12,32 @@ const AnosModelosPresentation = (props) => {
 
     return (
         <>
-            {!loading ? <Spinner />
-                :
-                <div class="row text-center" style={{ marginTop: "25px" }}>
-                    <div class="row slug-header">
-                        <div class="col-lg-2 col-md-4 col-sm-4 logo"><img src={marca.path_img} /></div>
-                        <div class="col-lg-6 col-md-4 com-sm-4 title">{marca.nome}</div>
-                        <div class="col-lg-4 col-md-4 com-sm-4 content">{modelo.nome}</div>
-                    </div>
-                    <table class="table table-striped table-bordered table-hover" style={{ border: "1px solid black" }}>
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Ano</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {anosModelos.map((anoModelo, key) => {
-                                let toUrl = `/marcas/${idMarca}/modelos/${idModelo}/anos/${anoModelo.codigo}`
-                                return (
-                                    <tr style={{ cursor: "pointer" }} onClick={() => window.location.href = toUrl}>
-                                        <td>{key + 1}</td>
-                                        <td><Link to={toUrl}>{anoModelo.nome}</Link></td>
-                                    </tr>
-                                )
-                            })}
-                        </tbody>
-                    </table>
+            <div class="row text-center" style={{ marginTop: "25px" }}>
+                <div class="row slug-header">
+                    <div class="col-lg-2 col-md-4 col-sm-4 logo"><img src={marca.path_img} /></div>
+                    <div class="col-lg-6 col-md-4 com-sm-4 title">{marca.nome}</div>
+                    <div class="col-lg-4 col-md-4 com-sm-4 content">{modelo.nome}</div>
                 </div>
-            }
+                <table class="table table-striped table-bordered table-hover" style={{ border: "1px solid black" }}>
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Ano</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {anosModelos.map((anoModelo, key) => {
+                            let toUrl = `/marcas/${idMarca}/modelos/${idModelo}/anos/${anoModelo.codigo}`
+                            return (
+                                <tr style={{ cursor: "pointer" }} onClick={() => window.location.href = toUrl}>
+                                    <td>{key + 1}</td>
+                                    <td><Link to={toUrl}>{anoModelo.nome}</Link></td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </div>
         </>
     )
 }
--- src/components/Carro/CarroPresentation.js
+++ src/components/Carro/CarroPresentation.js
@@ -8,29 +8,25 @@ const CarroPresentation = (props) => {
     var loading = props.loading
 
     return (
+
         <>
-            {!loading ? <Spinner />
-                :
-                <>
-                    <div class="row text-center" style={{ marginTop: "25px" }}>
-                        <div class="row slug-header">
-                            <div class="col-lg-2 col-md-4 col-sm-4 logo"><img src={marca.path_img} /></div>
-                            <div class="col-lg-6 col-md-4 com-sm-4 title">{marca.nome}</div>
-                            <div class="col-lg-4 col-md-4 com-sm-4 content">{carro.Modelo} ({carro.AnoModelo})</div>
-                        </div>
-                    </div>
-                    <div class="row text-center carro-presentation-container">
-                        <div class="carro-presentation-board">
-                            <div class="valor">{carro.Valor}</div>
-                            <div><b>Modelo:</b> {carro.Modelo} ({carro.Marca})</div>
-                            <div><b>Ano:</b> {carro.AnoModelo}</div>
-                            <div><b>Combustível:</b> {carro.Combustivel}</div>
-                            <div><b>Código Fipe:</b> {carro.CodigoFipe}</div>
-                            <div><b>Mês de Referência:</b> {carro.MesReferencia}</div>
-                        </div>
-                    </div>
-                </>
-            }
+            <div class="row text-center" style={{ marginTop: "25px" }}>
+                <div class="row slug-header">
+                    <div class="col-lg-2 col-md-4 col-sm-4 logo"><img src={marca.path_img} /></div>
+                    <div class="col-lg-6 col-md-4 com-sm-4 title">{marca.nome}</div>
+                    <div class="col-lg-4 col-md-4 com-sm-4 content">{carro.Modelo} ({carro.AnoModelo})</div>
+                </div>
+            </div>
+            <div class="row text-center carro-presentation-container">
+                <div class="carro-presentation-board">
+                    <div class="valor">{carro.Valor}</div>
+                    <div><b>Modelo:</b> {carro.Modelo} ({carro.Marca})</div>
+                    <div><b>Ano:</b> {carro.AnoModelo}</div>
+                    <div><b>Combustível:</b> {carro.Combustivel}</div>
+                    <div><b>Código Fipe:</b> {carro.CodigoFipe}</div>
+                    <div><b>Mês de Referência:</b> {carro.MesReferencia}</div>
+                </div>
+            </div>
         </>
     )
 }
